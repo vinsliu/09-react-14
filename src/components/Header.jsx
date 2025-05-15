71786bfe93193461eab406cac74c4a1b1420b7d8
Vincent <vincent00lav@outlook.com>
2025-05-15 15:46:01 +0200

    ✨ Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import { AppBar, Box, IconButton, MenuItem, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { purple } from "@mui/material/colors";
 
 function Header() {
+  const appBarBG = purple[800];
   return (
     <Box>
-      <AppBar>
+      <AppBar sx={{ backgroundColor: appBarBG }}>
         <Toolbar>
-          <IconButton>
-            <MenuItem />
+          <Typography sx={{ flexGrow: 1 }}>Tableau de bord</Typography>
+          <IconButton color="inherit">
+            <AccountCircleIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
